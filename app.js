e87a9fdf4081dd5f9faed15eaca3b754955bc724
Sergiyan92 <sergiyan@ukr.net>
2023-10-10 11:03:37 +0300

    education vue

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 new Vue({
   el: "#app",
   data: {
-    string: "Hello world",
+    people: [
+      {
+        name: "Franky Four Fingers",
+        stillAlive: false,
+      },
+      {
+        name: "Bullet-Tooth Tony",
+        stillAlive: false,
+      },
+      {
+        name: "Turkish",
+        stillAlive: true,
+      },
+      {
+        name: "Boris the Blade",
+        stillAlive: false,
+      },
+      {
+        name: "Mickey",
+        stillAlive: true,
+      },
+      {
+        name: "Brick Top",
+        stillAlive: false,
+      },
+    ],
   },
 });
